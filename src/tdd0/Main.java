147887f28b8320050f056a19c4608047831160fa
fodoucou <doucoure-f@hotmail.fr>
2016-09-27 14:35:49 +0200

    1 er commit : add main class 27_09_2016

---
+++ src/tdd0/Main.java
@@ -0,0 +1,11 @@
+package tdd0;
+
+/**
+ * Created by fodoucou on 27/09/2016.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("coucou");
+    }
+    
+}
